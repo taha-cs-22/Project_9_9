47bcc949fa06d5567981ae6b98eb21764601833b
Mohamed Ayman <hhogg914@gmail.com>
2025-12-13 20:36:09 +0200

    model , train baseline and results are ready

--- code/model.py
+++ code/model.py
@@ -2,7 +2,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import (
     Conv2D, MaxPooling2D,
     Flatten, Dense, Dropout,
+    GlobalAveragePooling2D
 )
+from tensorflow.keras.applications import MobileNet
 
 #Baseline
 
@@ -21,6 +23,26 @@ def build_baseline_model(input_shape, num_classes):
         Dense(256, activation="relu"),
         Dropout(0.5),
 
+        Dense(num_classes, activation="softmax")
+    ])
+    return model
+
+#MobileNet
+
+def build_mobilenet_model(input_shape, num_classes):
+    base_model = MobileNet(
+        input_shape=input_shape,
+        include_top=False,
+        weights="imagenet"
+    )
+
+    base_model.trainable = False
+
+    model = Sequential([
+        base_model,
+        GlobalAveragePooling2D(),
+        Dense(128, activation="relu"),
+        Dropout(0.3),
         Dense(num_classes, activation="softmax")
     ])
     return model
